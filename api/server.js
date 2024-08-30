f33c37ef7b795c01c1c086687441566b8fac8d1f
Abhay Dixit <dixitshivam534@gmail.com>
2024-08-30 10:38:18 +0530

    updated server.js

--- api/server.js
+++ api/server.js
@@ -1,24 +1,26 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
+import { create, router as _router, defaults, rewriter } from 'json-server'
+import { readFileSync } from 'fs'
+import { join } from 'path'
 
-const server = jsonServer.create()
+// ...
 
-// Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
+const server = create()
 
-// Comment out to allow write operations
-const router = jsonServer.router('db.json')
+// Allow write operations
+const filePath = join('db.json')
+const data = readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = _router(db)
 
-const middlewares = jsonServer.defaults()
+// Comment out to prevent read-only file system error
+// const router = jsonServer.router('db.json')
+
+const middlewares = defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
+server.use(rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
@@ -28,4 +30,58 @@ server.listen(3000, () => {
 })
 
 // Export the Server API
-module.exports = server
+export default server
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // See https://github.com/typicode/json-server#module
+// const jsonServer = require('json-server')
+
+// const server = jsonServer.create()
+
+// // Uncomment to allow write operations
+// // const fs = require('fs')
+// // const path = require('path')
+// // const filePath = path.join('db.json')
+// // const data = fs.readFileSync(filePath, "utf-8");
+// // const db = JSON.parse(data);
+// // const router = jsonServer.router(db)
+
+// // Comment out to allow write operations
+// const router = jsonServer.router('db.json')
+
+// const middlewares = jsonServer.defaults()
+
+// server.use(middlewares)
+// // Add this before server.use(router)
+// server.use(jsonServer.rewriter({
+//     '/api/*': '/$1',
+//     '/blog/:resource/:id/show': '/:resource/:id'
+// }))
+// server.use(router)
+// server.listen(3000, () => {
+//     console.log('JSON Server is running')
+// })
+
+// // Export the Server API
+// module.exports = server
